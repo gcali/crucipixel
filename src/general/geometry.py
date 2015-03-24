143256e8c5a3790b1a4cdf292d8af37c70113914
Giovanni Calì <gcali.git@gmail.com>
2015-03-24 21:15:26 +0100

    Add copy to Point

--- src/general/geometry.py
+++ src/general/geometry.py
@@ -20,6 +20,9 @@ class Point:
     def row(self):
         return self.y 
     
+    def copy(self):
+        return Point(self.x,self.y)
+    
     def __add__(self,other):
         return Point(self.x + other.x, self.y + other.y)
     
